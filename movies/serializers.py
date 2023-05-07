fab3ac950b60823c9e7e4f7a71b9a82d5a47dc5d
soeun Jang <azulcielo_@naver.com>
2023-05-07 22:45:04 +0900

    first commit

---
+++ movies/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Movie, Actor
+
+class MovieSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+    opening_date = serializers.DateField()
+    running_time = serializers.IntegerField()
+    overview = serializers.CharField()
+
+class ActorSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+    gender = serializers.CharField()
+    birth_date = serializers.DateField()
